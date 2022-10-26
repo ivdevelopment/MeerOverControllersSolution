905b06b3fd130d774248b361cc43a332a0937283
Ian Verlinden <ianverlinden1.iv@gmail.com>
2022-10-26 13:32:44 +0200

    Add project files.

---
+++ MeerOverControllers/Controllers/WerknemerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeerOverControllers.Controllers
+{
+    public class WerknemerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [NonAction]
+        public string HuidigeDatum()
+        {
+            return DateTime.Now.ToShortDateString();
+        }
+    }
+}
