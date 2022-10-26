aa41b42bb6280908c0e04e1a32093b7e4c375a4d
Ian Verlinden <ianverlinden1.iv@gmail.com>
2022-10-26 14:53:19 +0200

    .

--- MeerOverControllers/Controllers/WerknemerController.cs
+++ MeerOverControllers/Controllers/WerknemerController.cs
@@ -6,7 +6,43 @@ namespace MeerOverControllers.Controllers
     {
         public IActionResult Index()
         {
+            /*return Content("Kiekeboe");*/
+            return File(@"images\happy.png", "image/png");
+        }
+
+        /*public IActionResult Detail()
+        {
             return View();
+        }*/
+
+        public IActionResult Detail(int? id)
+        {
+            return View();
+        }
+
+        public IActionResult VerdubbelDeWeddes()
+        {
+            // update in de database
+            //.....
+            return Redirect("~/Werknemer/WeddesAangepast");
+        }
+
+        public IActionResult WeddesAangepast()
+        {
+            return View();
+        }
+
+        public IActionResult EvenOfOneven(int id)
+        {
+            if (id % 2 == 0)
+                return View("Even");
+            else
+                return View("Oneven");
+        }
+
+        public IActionResult Alfa()
+        {
+            return View("Beta");
         }
 
         [NonAction]
